4d09692a966a0258046a8359ec0cccd9b1804210
Nicat.dcw <59221034+Nicat-dcw@users.noreply.github.com>
2021-04-19 11:53:29 +0400

    Update kayıt.js

--- kayıt.js
+++ kayıt.js
@@ -2,10 +2,10 @@
 const Discord = require('discord.js')
 const Database = require('plasma-db')
 const db = new Database('./kayıt.json')
-exports.run = async(client,message,args) => {
+exports.run = async(client, message, args) => {
 var kontrol1 = await db.fetch(`kayitsistem_${message.guild.id}`) 
 var kontrol2 = await db.fetch(`kayitsistemrol_${message.guild.id}`) 
-var kontrol3 = await db.fetch(`kayitsistemikanal_${message.guild.id}`)
+var kontrol3 = await db.get(`kayitsistemikanal_${message.guild.id}`)
 if(!kontrol3) return message.channel.send('Kayıt Kanalından Kayit Olmalisin!')
 if(kontrol3 == null) return message.channel.send('')
 if(message.channel.id !== kontrol3) return message.channel.send('Kayıt Kanalından Kayıt Olman Lazım :c')
@@ -22,11 +22,16 @@ setTimeout(function(){
 name.roles.add(db.fetch(`kayıtsistemirol_${message.guild.id}`))},2000)
  name.setNickname(`${ad} \ ${yas}`)
 }
+/*
 exports.conf =  {
 enabled: true,
 permLevel: 4,
 aliases: []
-};
+};*/
 exports.help = {
-name: "kayıt"
+name: "kayıt",
+description: "",
+usage: "",
+aliases: [],
+kategori: "mod"
 }
